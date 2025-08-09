183b891e57247e21e8460a50ef33b3cbdef30ac3
Cipriano <cipriano.guilherme@icloud.com>
2025-08-09 23:57:59 +0900

    first commit

---
+++ src/main/java/com/cipriano/yosan/YosanApplication.java
@@ -0,0 +1,13 @@
+package com.cipriano.yosan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YosanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cipriano/yosan/YosanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cipriano.yosan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YosanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
