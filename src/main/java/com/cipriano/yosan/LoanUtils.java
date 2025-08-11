553d68a070eaba60c96e7b3e6d2afb7f95c79b93
Cipriano <cipriano.guilherme@icloud.com>
2025-08-11 19:04:58 +0900

    refatoracao da classe, correcao de bugs da funcao

--- src/main/java/com/cipriano/yosan/LoanUtils.java
+++ src/main/java/com/cipriano/yosan/LoanUtils.java
@@ -4,86 +4,120 @@ import com.cipriano.yosan.dto.LoanInstallment;
 import com.cipriano.yosan.dto.request.LoanRequest;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Locale;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
 public abstract class LoanUtils {
-
-    private static final DecimalFormatSymbols symbols = new DecimalFormatSymbols(new Locale("pt", "BR"));
-    private static final DecimalFormat df = new DecimalFormat("#,##0.00", symbols);
     public static final int MONTHS_IN_A_YEAR = 12;
-    public static final int DAYS_IN_A_YEAR = 365;
+    public static final BigDecimal BIG_DECIMAL_MONTHS_IN_A_YEAR = new BigDecimal(12);
+    public static final BigDecimal BIG_DECIMAL_DAYS_IN_A_YEAR = new BigDecimal(365);
+    public static final int MONETARY_SCALE = 2;
+    public static final int TAX_SCALE = 10;
+    public static final BigDecimal BIG_DECIMAL_TAX_RATE_BASE = new BigDecimal(100);
+    public static final int FIRST_INSTALLMENT = 0;
 
-    public static ArrayList<LoanInstallment>  calculateLoanSAC(LoanRequest loanRequest) {
-
+    public static ArrayList<LoanInstallment> calculateLoanSAC(LoanRequest loanRequest) {
         ArrayList<LoanInstallment> loanInstallmentList = new ArrayList<>();
 
         int months = getTotalMonthLoan(loanRequest);
-        float[] installments = new float[months];
 
-        float MonthlyPaidDebtValue = Float.parseFloat(String.valueOf(loanRequest.getLoanValue())) / (float) months;
-        float remainingBalance = Float.parseFloat(String.valueOf(loanRequest.getLoanValue()));
+        if (months <= 0) {
+            return new ArrayList<>();
+        }
 
+        BigDecimal monthsBigDecimal = BigDecimal.valueOf(months);
+        BigDecimal monthlyAmortization = loanRequest.getLoanValue().divide(monthsBigDecimal, MONETARY_SCALE, RoundingMode.HALF_UP);
+        BigDecimal remainingBalance = loanRequest.getLoanValue();
+        BigDecimal gracePeriodDays = getGracePeriodDays(loanRequest);
 
+        LocalDate currentPaymentDate = loanRequest.getFirstPaymentDate();
 
-        for (int installmentNumber = 0; installmentNumber < months; installmentNumber++) {
-            float taxValue = 0f;
+        for (int installment = 0; installment < months; installment++) {
+            int installmentNumber = installment + 1;
 
-            if (installmentNumber == 0) {
-                taxValue =  getDailyTaxValue(String.valueOf(loanRequest.getTaxRate()), remainingBalance, getTotalGraceDays(loanRequest));
+            BigDecimal taxValue;
+
+            if (isLastInstallment(installment, FIRST_INSTALLMENT)) {
+                taxValue = getFirstMonthTax(loanRequest, remainingBalance, gracePeriodDays);
             } else {
-                taxValue = getMonthlyTaxValue(String.valueOf(loanRequest.getTaxRate()), remainingBalance);
+                taxValue = getOtherMonthTax(loanRequest, remainingBalance, gracePeriodDays);
             }
 
-            float installmentValue = MonthlyPaidDebtValue + taxValue;
+            if (isLastInstallment(installmentNumber, months)) {
+                monthlyAmortization = remainingBalance;
+            }
 
-            installments[installmentNumber] = installmentValue;
+            BigDecimal installmentValue = monthlyAmortization.add(taxValue);
+            remainingBalance = remainingBalance.subtract(monthlyAmortization);
+            LocalDate finalInstallmentDate = currentPaymentDate;
 
-
-            remainingBalance -= MonthlyPaidDebtValue;
+            if (isLastInstallment(installmentNumber, months)) {
+                finalInstallmentDate = loanRequest.getEndDate();
+            }
 
             loanInstallmentList.add(
                     LoanInstallment.builder()
-                            .installmentNumber(String.valueOf(installmentNumber+1))
-                            .taxValue(BigDecimal.valueOf(taxValue))
-                            .paidDebtValue(BigDecimal.valueOf(MonthlyPaidDebtValue))
-                            .installmentValue(BigDecimal.valueOf(installmentValue))
-                            .remainingBalance(BigDecimal.valueOf(remainingBalance))
-                            .date(loanRequest.getFirstPaymentDate().plusMonths(installmentNumber))
+                            .installmentNumber(String.valueOf(installmentNumber))
+                            .taxValue(taxValue)
+                            .paidDebtValue(monthlyAmortization)
+                            .installmentValue(installmentValue)
+                            .remainingBalance(remainingBalance)
+                            .date(finalInstallmentDate)
                             .build()
             );
 
+            if (isPaymentOnLastDayOfMonth(loanRequest)) {
+                currentPaymentDate = currentPaymentDate.plusMonths(1).with(TemporalAdjusters.lastDayOfMonth());
+            } else {
+                currentPaymentDate = currentPaymentDate.plusMonths(1);
+            }
         }
 
         return loanInstallmentList;
     }
 
-    public static String formatValue(double valor) {
-        return df.format(valor);
+    private static boolean isLastInstallment(int installmentNumber, int months) {
+        return installmentNumber == months;
     }
 
-    private static float getDailyTaxValue(String taxRate, float remainingBalance, float gracePeriodDays) {
-        return remainingBalance * (Float.parseFloat(taxRate) / 100.0F / DAYS_IN_A_YEAR) * gracePeriodDays;
-
+    private static BigDecimal getMonthlyTaxRate(LoanRequest loanRequest) {
+        return loanRequest.getTaxRate()
+                .divide(BIG_DECIMAL_TAX_RATE_BASE, TAX_SCALE, RoundingMode.HALF_UP)
+                .divide(BIG_DECIMAL_MONTHS_IN_A_YEAR, TAX_SCALE, RoundingMode.HALF_UP);
     }
 
-    private static float getMonthlyTaxValue(String taxRate, float remainingBalance) {
-        return remainingBalance * (Float.parseFloat(taxRate) / 100.0F / MONTHS_IN_A_YEAR);
-
+    private static BigDecimal getDailyTaxRate(LoanRequest loanRequest) {
+       return loanRequest.getTaxRate()
+                .divide(BIG_DECIMAL_TAX_RATE_BASE, TAX_SCALE, RoundingMode.HALF_UP)
+                .divide(BIG_DECIMAL_DAYS_IN_A_YEAR, TAX_SCALE, RoundingMode.HALF_UP);
     }
 
-    private static float getTotalGraceDays(LoanRequest loanRequest) {
-        return (float) ChronoUnit.DAYS.between(loanRequest.getInitialDate(), loanRequest.getFirstPaymentDate());
+    private static BigDecimal getFirstMonthTax(LoanRequest loanRequest, BigDecimal remainingBalance, BigDecimal gracePeriodDays) {
+       return remainingBalance.multiply(
+               getDailyTaxRate(loanRequest)
+       ).multiply(gracePeriodDays);
+    }
 
+    private static BigDecimal getOtherMonthTax(LoanRequest loanRequest, BigDecimal remainingBalance, BigDecimal gracePeriodDays) {
+        return remainingBalance.multiply(
+                getMonthlyTaxRate(loanRequest)
+        ).multiply(gracePeriodDays);
+    }
+
+    private static boolean isPaymentOnLastDayOfMonth(LoanRequest loanRequest) {
+        return loanRequest.getFirstPaymentDate().getDayOfMonth() == loanRequest.getFirstPaymentDate().lengthOfMonth();
+    }
+
+    private static BigDecimal getGracePeriodDays(LoanRequest loanRequest) {
+        return new BigDecimal(ChronoUnit.DAYS.between(loanRequest.getInitialDate(), loanRequest.getFirstPaymentDate()));
     }
 
     private static int getTotalMonthLoan(LoanRequest loanRequest) {
         Period period = Period.between(loanRequest.getInitialDate(), loanRequest.getEndDate());
         return period.getYears() * MONTHS_IN_A_YEAR + period.getMonths();
-
     }
 }
