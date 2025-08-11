83bdeefaa216f0c5356c2ba33a824ae27274d114
Cipriano <cipriano.guilherme@icloud.com>
2025-08-11 16:59:40 +0900

    tratamento de erros

--- src/main/java/com/cipriano/yosan/controller/LoanCalculatorController.java
+++ src/main/java/com/cipriano/yosan/controller/LoanCalculatorController.java
@@ -3,23 +3,39 @@ package com.cipriano.yosan.controller;
 import com.cipriano.yosan.dto.request.LoanRequest;
 import com.cipriano.yosan.dto.response.LoanResponse;
 import com.cipriano.yosan.service.LoanServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/yosan")
 public class LoanCalculatorController {
 
-    LoanCalculatorController() {
-        this.loanServiceImpl = new LoanServiceImpl();
+    LoanServiceImpl loanServiceImpl;
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoanCalculatorController.class);
+
+    LoanCalculatorController(LoanServiceImpl loanServiceImpl) {
+        this.loanServiceImpl = loanServiceImpl;
     }
 
-    LoanServiceImpl loanServiceImpl;
-
-
     @PostMapping("/calculate-sac-loan")
-    LoanResponse calculateSACLoan(@RequestBody LoanRequest loanRequest) throws InterruptedException {
-        Thread.sleep(1000);
-        return this.loanServiceImpl.calculateLoan(loanRequest);
+    ResponseEntity<LoanResponse> calculateSACLoan(@RequestBody LoanRequest loanRequest) throws InterruptedException {
+        try {
+            Thread.sleep(1000);
+            LoanResponse responseBody = this.loanServiceImpl.calculateLoan(loanRequest);
+            return ResponseEntity.ok(responseBody);
+        } catch (IllegalArgumentException e) {
+            LOGGER.warn("Invalid data, plase chack: {}", e.getMessage());
+            return ResponseEntity.badRequest().build();
+        } catch (InterruptedException e) {
+            LOGGER.warn("Interrupted while calculating loan: {}", e.getMessage());
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch(Exception e) {
+            LOGGER.warn("Unknown error: {}", e.getMessage());
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
